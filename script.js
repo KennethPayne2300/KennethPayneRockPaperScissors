4ac331fb9cdd2fcfd587ca04b8f5fa16fbda17c3
KennethPayne2300 <kennethpayne353@gmail.com>
2025-06-11 12:46:45 -0500

    finishing

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const player1 = document.getElementById("one")
-const player2 = document.getElementById("two")
+const player1 = document.querySelector(".one");
+const player2 = document.querySelector(".two");
+const status = document.getElementById('status');
 
-// console.log(player1, player2)
-var one
-var two
-var three
+// console.log(player1, player2, status)
+let one;
+let two;
+let three;
+let play1;
+let play2;
 
 document.addEventListener("keydown", function(event) {
     if(event.key === "1"){
@@ -25,20 +28,57 @@ document.addEventListener("keyup", function(event) {
     }else if (event.key === "3"){
         three = false;
     }
-    keypress()
 });
 
 function keypress() {
     if (one){
-        player1.src = 'assets/Rock.png'
+        player1.src = 'assets/Rock.png';
+        play1 = 'rock';
     }else if (two){
-        player1.src = 'assets/Paper.png'
+        player1.src = 'assets/Paper.png';
+        play1 = 'paper';
     }else if (three){
-        player1.src = 'assets/Scissors.png'
-    }
-    aipress()
-}
+        player1.src = 'assets/Scissors.png';
+        play1 = 'scissors';
+    };
+        aipress();
+};
 
 function aipress() {
+    let randomnumber = Math.floor(Math.random()*3)+1;
+    // console.log(randomnumber)
+    if(randomnumber == 1){
+        player2.src = 'assets/Rock.png';
+        play2 = 'rock';
+    }else if(randomnumber == 2){
+        player2.src = 'assets/Paper.png';
+        play2 = 'paper';
+    }else if(randomnumber == 3){
+        player2.src = 'assets/Scissors.png';
+        play2 = 'scissors';
+    };
+
+    // console.log(player1.src, player2.src);
     
+    if(play1 == play2){
+        status.innerHTML = "Its a tie!!";
+    }else if(play1 == 'rock'){
+        if(play2 == 'scissors'){
+            status.innerHTML = "Congratulations you won!!";
+        }else if(play2 == 'Paper'){
+            status.innerHTML = "Sorry you lost";
+        }
+    }else if(play1 == 'Paper'){
+        if(player2.src == 'assets/Rock.png'){
+            status.innerHTML = "Congratulations you won!!";
+        }else if (player2.src == 'assets/Scissors.png'){
+            status.innerHTML = "Sorry you lost";
+        }
+    }else if(player1.src == 'assets/Scissors.png'){
+        if(player2.src == 'assets/Paper.png'){
+            status.innerHTML = "Congratulations you won!!";
+        }else if (player2.src == 'assets/Rock.png'){
+            status.innerHTML = "Sorry you lost";
+        }
+    }
 }
